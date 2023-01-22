0b10c780bfb8b9181050612d12f6cebcd940f9af
unknown <unknown>
2023-01-22 23:22:08 +0200

    Added set mapper

--- domain/export/json-schema/column-mapper/index.js
+++ domain/export/json-schema/column-mapper/index.js
@@ -3,9 +3,11 @@ import {SimpleColumnMapper} from "./simple-column-mapper.js";
 import {SampledStringColumnMapper} from "./sampled-string-column-mapper.js";
 import {ArrayColumnMapper} from "./array-column-mapper.js";
 import {FrozenColumnMapper} from "./frozen-column-mapper.js";
+import {SetColumnMapper} from "./set-column-mapper.js";
 
 const baseMapper = new BaseColumnMapper(undefined);
-const arrayMapper = new ArrayColumnMapper(baseMapper);
+const setMapper = new SetColumnMapper(baseMapper);
+const arrayMapper = new ArrayColumnMapper(setMapper);
 const frozenMapper = new FrozenColumnMapper(arrayMapper);
 const textMapper = new SampledStringColumnMapper(frozenMapper);
 const simpleMapper = new SimpleColumnMapper(textMapper);
--- domain/export/json-schema/column-mapper/set-column-mapper.js
+++ domain/export/json-schema/column-mapper/set-column-mapper.js
@@ -1,1 +1,41 @@
+import {BaseColumnMapper} from "./base-column-mapper.js";
+import {NestedColumnEntity} from "../../../database/model/nested-column-entity.js";
+import {ColumnTypes} from "../../../database/model/enums/column-type-enum.js";
+import {JsonSchemaTypes} from "../enums/json-schema-types.js";
+import {ColumnMapper} from "./index.js";
 
+
+export class SetColumnMapper extends BaseColumnMapper {
+
+    /**
+     * @param nextMapper {BaseColumnMapper | undefined}
+     * */
+    constructor(nextMapper) {
+        super(nextMapper);
+    }
+
+
+    mapColumn(column) {
+        this._assertColumnValid(column);
+        if (column instanceof NestedColumnEntity && column.columnType === ColumnTypes.SET) {
+            if (column.children.length !== 1) {
+                throw new Error(`Invalid column: set column must have exactly 1 child, got: ${JSON.stringify(column)}`)
+            }
+            const child = column.children[0];
+            const childJsonSchema = ColumnMapper.mapColumn(child);
+            const jsonSchema = {
+                type: JsonSchemaTypes.ARRAY,
+                uniqueItems: true,
+                items: childJsonSchema,
+            };
+            if (column.name) {
+                return {
+                    [column.name]: jsonSchema
+                }
+            }
+            return jsonSchema;
+        }
+        return this._next(column);
+    }
+
+}
